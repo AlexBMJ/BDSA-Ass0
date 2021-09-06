885cc310765f230cf7a64f991153fae60f4f96af
AlexBMJ <33891167+AlexBMJ@users.noreply.github.com>
2021-09-07 01:56:00 +0200

    Feature/console app (#1)
    * added projects to solution
    * Tested and implemented IsLeapYear

---
+++ ConsoleApp/ConsoleProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp {
+    public class ConsoleProgram {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsLeapYear(int year) {
+            return ((IsDivisibleByFour(year) && !IsDivisibleByOneHundred(year)) || IsDivisibleByFourHundred(year));
+        }
+
+        public bool IsDivisibleByFour(int num) {
+            return (num % 4) == 0;
+        }
+        public bool IsDivisibleByOneHundred(int num) {
+            return (num % 100) == 0;
+        }
+
+        public bool IsDivisibleByFourHundred(int num) {
+            return (num % 400) == 0;
+        }
+    }
+}
